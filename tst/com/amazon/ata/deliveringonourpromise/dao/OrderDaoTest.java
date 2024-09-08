1f3625da9bbac1b03a469d26000e079f98c1b6d6
Prince Slater <everythingindmoderation2@gmail.com>
2024-09-08 18:54:00 -0400

    OrderDaoTest

--- tst/com/amazon/ata/deliveringonourpromise/dao/OrderDaoTest.java
+++ tst/com/amazon/ata/deliveringonourpromise/dao/OrderDaoTest.java
@@ -1,12 +1,32 @@
 package com.amazon.ata.deliveringonourpromise.dao;
 
+import com.amazon.ata.deliveringonourpromise.App;
+import com.amazon.ata.deliveringonourpromise.ordermanipulationauthority.OrderManipulationAuthorityClient;
+import com.amazon.ata.deliveringonourpromise.types.Order;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class OrderDaoTest {
+    private OrderManipulationAuthorityClient omaClient = App.getOrderManipulationAuthorityClient();
+    private OrderDao dao = new OrderDao(omaClient);
 
-    private OrderDao dao;
+    @Test
+    //Happy case, verifying that the OrderDao can return an order.
+    public void get_forKnownOrderId_returnsOrder() {
+        //Given
+        //An order ID that we know exist
+        String orderId = "111-7497023-2960776";
+
+        // When
+        //We call get() with that order ID
+        Order result = dao.get(orderId);
+
+        //Then
+        //The result is not null
+        assertNotNull(result, "An existing order should be returned.");
+    }
+
 
 }
 
